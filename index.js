d68891045b15e1800acb4c8a5cd8144f148c4b4a
sabrinaflorenco <sabrinaflorenco@bol.com.br>
2024-01-12 10:25:14 -0300

    feat:arquivo html e js

---
+++ index.js
@@ -0,0 +1,24 @@
+function nivelJogador(vitorias,derrotads){
+    let saldoVitorias = vitorias - derrotads;
+    let nivel = "";
+
+    if (saldoVitorias < 10){
+        nivel = "Ferro";
+    }else if (saldoVitorias >= 10 && saldoVitorias <= 20) {
+    	nivel = "Bronze";
+    }else if (saldoVitorias >= 21 && saldoVitorias <= 50) {
+    	nivel = "Prata";
+    }else if (saldoVitorias >= 51 && saldoVitorias <= 80) {
+    	nivel = "Ouro";
+    }else if (saldoVitorias >= 81 && saldoVitorias <= 90) {
+    	nivel = "Diamante";
+    }else if (saldoVitorias >= 91 && saldoVitorias <= 100) {
+    	nivel = "Lendário";
+    } else if (saldoVitorias >= 101) {
+       nivel = "Imortal";
+    }
+    return { saldoVitorias, nivel };
+    }
+
+let resultado = nivelJogador(50, 5);
+console.log(`O Herói tem de saldo de ${resultado.saldoVitorias} está no nível de ${resultado.nivel}`);
